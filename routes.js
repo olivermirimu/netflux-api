14484e4f300f892f2f7e3e869b6a6abe066ff72d
olivermirimu <olivermirimu@gamil.com>
2020-12-14 21:13:08 +0300

    migrated db to mongo atlas

--- routes.js
+++ routes.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 function routes(Movie, User) {
   const apiRouter = express.Router();
 
   /***User Routes***/
-  apiRouter.route('/users')
+  apiRouter
+    .route("/users")
     .post((req, res) => {
+      // TODO: Bar creation of multiple accounts using same email
       const user = new User(req.body);
-      user.save().catch(err => {
+      user.save().catch((err) => {
         console.log(err);
       });
       return res.status(201).json(user);
@@ -21,86 +23,81 @@ function routes(Movie, User) {
       });
     });
 
-  apiRouter.use('/user/:email', (req, res, next) => {
-    User.find({
-      email: req.params.email
-    }, (err, user) => {
-      if (err) {
-        return res.send(err);
+  apiRouter.use("/user/:email", (req, res, next) => {
+    User.find(
+      {
+        email: req.params.email,
+      },
+      (err, user) => {
+        if (err) {
+          return res.send(err);
+        }
+        if (user) {
+          req.user = user;
+          return next();
+        }
+        return res.sendStatus(404);
       }
-      if (user) {
-        req.user = user;
-        return next();
-      }
-      return res.sendStatus(404);
-    });
+    );
   });
   //TODO: to be changed to username or email or third party verification
   //TODO: put and patch methods failing at save
 
-  apiRouter.route('/user/:email')
+  apiRouter
+    .route("/user/:email")
     .get((req, res) => res.json(req.user))
     .put((req, res) => {
-      User.findOneAndReplace({
-        email: req.params.email
-      }, req.user, (err, user) => {
-        if (err) {
-          return res.send(err);
+      User.findOneAndReplace(
+        {
+          email: req.params.email,
+        },
+        req.user,
+        (err, user) => {
+          if (err) {
+            return res.send(err);
+          }
+          return res.json(user);
         }
-        return res.json(user);
-      });
+      );
     })
     .patch((req, res) => {
-      User.findOneAndUpdate({
-        email: req.params.email
-      }, req.body, (err, user) => {
-        if (err) {
-          return res.send(err);
-        }
-        console.log(user);
-        return res.json(user);
-      });
-      // const {
-      //   user
-      // } = req;
-      // if (req.body._id) {
-      //   delete req.body._id;
-      // }
-      // Object.entries(req.body).forEach(item => {
-      //   const key = item[0];
-      //   const value = item[1];
-      //   user[key] = value;
-      // });
-      // console.log(req.user);
-      // try {
-      //   req.user.save();
-      //   return res.json(user);
-      // } catch (err) {
-      //   res.send(err);
-      // }
+      User.findOneAndUpdate(
+        {
+          email: req.params.email,
+        },
+        req.body,
+        { new: true }
+      )
+        .then((_newUser) => {
+          return _newUser ? res.json(_newUser) : res.sendStatus(404);
+        })
+        .catch((err) => res.send(err));
     })
     .delete((req, res) => {
       User.findOneAndDelete({
-        email: req.params.email
-      }, err => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.sendStatus(204);
-      });
+        email: req.params.email,
+      })
+        .then((deletedUser) =>
+          // TODO: throw error for request to delete none-existent account
+          res.send(`Deletion Of ${deletedUser.email} Account Successfull: `)
+        )
+        .catch((err) => res.send(err));
     });
 
+  /*********AUTHENTICATION*********/
+  
+
   /***Movie Routes***/
-  apiRouter.route('/movies')
+  apiRouter
+    .route("/movies")
     .post((req, res) => {
       const movie = new Movie(req.body);
-      movie.save().catch(err => {
+      movie.save().catch((err) => {
         console.log(err);
       });
       return res.status(201).json(movie);
     })
     .get((req, res) => {
-
       Movie.find((err, movies) => {
         if (err) {
           return res.send(err);
@@ -108,7 +105,7 @@ function routes(Movie, User) {
         const returnMovies = movies.map((movie) => {
           let newMovie = movie.toJSON();
           newMovie.links = {};
-          const title = movie.title.split(' ').join('%20');
+          const title = movie.title.split(" ").join("%20");
           newMovie.links.self = `http://${req.headers.host}/api/movie/${title}`;
           return newMovie;
         });
@@ -116,55 +113,62 @@ function routes(Movie, User) {
       });
     });
 
-  apiRouter.route('/movie/:title')
-    .get((req, res) => {
-      Movie.find({
-        title: req.params.title
-      }, (err, movies) => {
+  apiRouter.route("/movie/:title").get((req, res) => {
+    Movie.find(
+      {
+        title: req.params.title,
+      },
+      (err, movies) => {
         if (err) {
           return res.send(err);
         }
         return res.json(movies);
-      });
-    });
-  /*get by genre*/
-  apiRouter.route('/genre/:genre').get((req, res) => {
-    Movie.find({
-      genre: req.params.genre
-    }, (err, movies) => {
-      if (err) {
-        return res.send(err);
       }
-      const retrunMovies = movies.map(movie => {
-        let newMovie = movie.toJSON();
-        newMovie.links = {};
-        const title = movie.title.split(' ').join('%20');
-        newMovie.links.self = `http://${req.headers.host}/api/movie/${title}`;
-        return newMovie;
-      });
-      return res.json(retrunMovies);
-    });
+    );
+  });
+  /*get by genre*/
+  apiRouter.route("/genre/:genre").get((req, res) => {
+    Movie.find(
+      {
+        genre: req.params.genre,
+      },
+      (err, movies) => {
+        if (err) {
+          return res.send(err);
+        }
+        const retrunMovies = movies.map((movie) => {
+          let newMovie = movie.toJSON();
+          newMovie.links = {};
+          const title = movie.title.split(" ").join("%20");
+          newMovie.links.self = `http://${req.headers.host}/api/movie/${title}`;
+          return newMovie;
+        });
+        return res.json(retrunMovies);
+      }
+    );
   });
   /*search*/
-  apiRouter.route('/search/:searchTerm').get((req, res) => {
+  apiRouter.route("/search/:searchTerm").get((req, res) => {
     const searchTerm = req.params.searchTerm;
-    Movie.find({
-      title: /searchTerm/
-
-    }, (err, movies) => {
-      if (err) {
-        return res.send(err);
+    Movie.find(
+      {
+        title: /searchTerm/,
+      },
+      (err, movies) => {
+        if (err) {
+          return res.send(err);
+        }
+        const returnMovies = movies.map((movie) => {
+          let newMovie = movie.toJSON();
+          newMovie.links = {};
+          const title = movie.title.split(" ").join("%20");
+          newMovie.links.self = `http://${req.headers.host}/api/movie/${title}`;
+          return newMovie;
+        });
+        return res.json(returnMovies);
+        // res.send();
       }
-      const returnMovies = movies.map(movie => {
-        let newMovie = movie.toJSON();
-        newMovie.links = {};
-        const title = movie.title.split(' ').join('%20');
-        newMovie.links.self = `http://${req.headers.host}/api/movie/${title}`;
-        return newMovie;
-      });
-      return res.json(returnMovies);
-      res.send();
-    });
+    );
   });
   return apiRouter;
 }
--- server.js
+++ server.js
@@ -1,46 +1,59 @@
-const express = require('express');
-const chalk = require('chalk');
-const debug = require('debug')('server');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+/*jslint es6 */
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("server");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const Movie = require('./models/movieModel');
-const User = require('./models/userModel');
-const MONGODB_URI = 'mongodb://heroku_vqqc1kts:cue8h98gpu4p3368j3lmr5d1r1@ds145356.mlab.com:45356/heroku_vqqc1kts' || process.env.MONGODB_URI || 'mongodb: //localhost/movieApi';
-const db = mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, err => {
-  if (err) {
-    console.log(`there is a prolem`, err);
-  } else {
-    console.log(`Your connection is ready`);
+const Movie = require("./models/movieModel");
+const User = require("./models/userModel");
+const MONGODB_URI =
+  "mongodb+srv://Nico:niconetflux@netflux.pwqcz.mongodb.net/netflux?retryWrites=true&w=majority" ||
+  "mongodb://localhost/movieApi";
+// "mongodb://heroku_vqqc1kts:cue8h98gpu4p3368j3lmr5d1r1@ds145356.mlab.com:45356/heroku_vqqc1kts" ||
+// "mongodb://localhost/movieApi" ||
+// process.env.MONGODB_URI;
+const db = mongoose.connect(
+  MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    err
+      ? console.log(`there is a prolem`, err.message)
+      : console.log(`Your connection is ready`);
   }
-});
+);
 mongoose.connection;
 
 const app = express();
 const port = process.env.PORT || 3500;
-const apiRouter = require('./routes')(Movie, User);
+const apiRouter = require("./routes")(Movie, User);
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use((req, res, next) => {
   res.header(`Access-Control-Allow-Origin`, `*`);
-  res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept, Authorization`);
+  res.header(
+    `Access-Control-Allow-Headers`,
+    `Origin, X-Requested-With, Content-Type, Accept, Authorization`
+  );
   next();
 });
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
 //routing ->routes.js
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-app.get('/', function (req, res) {
-  res.send('Welcome to my API');
+app.get("/", function (req, res) {
+  res.send("Welcome to my API");
 });
 
 app.listen(port, function () {
